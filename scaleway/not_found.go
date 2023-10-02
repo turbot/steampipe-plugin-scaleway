4ca33874a23d6149543c13bc2f84c24a508783af
Ved misra <47312748+misraved@users.noreply.github.com>
2023-10-02 10:49:20 +0530

    Recompile plugin with steampipe-plugin-sdk v5.6.1 and Go version 1.21 (#41)

--- scaleway/not_found.go
+++ scaleway/not_found.go
@@ -1,15 +1,15 @@
 package scaleway
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/scaleway/scaleway-sdk-go/scw"
-	"golang.org/x/xerrors"
 )
 
 func is404Error(err error) bool {
 	notFoundError := &scw.ResourceNotFoundError{}
-	return isHTTPCodeError(err, http.StatusNotFound) || xerrors.As(err, &notFoundError)
+	return isHTTPCodeError(err, http.StatusNotFound) || errors.As(err, &notFoundError)
 }
 
 func isHTTPCodeError(err error, statusCode int) bool {
@@ -18,7 +18,7 @@ func isHTTPCodeError(err error, statusCode int) bool {
 	}
 
 	responseError := &scw.ResponseError{}
-	if xerrors.As(err, &responseError) && responseError.StatusCode == statusCode {
+	if errors.As(err, &responseError) && responseError.StatusCode == statusCode {
 		return true
 	}
 	return false
