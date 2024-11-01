1d2b286844ca832539e7502d6878d61e39fec1f9
tdannenmuller <114987187+tdannenmuller@users.noreply.github.com>
2024-11-01 14:57:27 +0100

    add Invoices table

--- scaleway/plugin.go
+++ scaleway/plugin.go
@@ -31,6 +31,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"scaleway_instance_server":         tableScalewayInstanceServer(ctx),
 			"scaleway_instance_snapshot":       tableScalewayInstanceSnapshot(ctx),
 			"scaleway_instance_volume":         tableScalewayInstanceVolume(ctx),
+			"scaleway_invoices":                tableScalewayInvoices(ctx),
 			"scaleway_kubernetes_cluster":      tableScalewayKubernetesCluster(ctx),
 			"scaleway_kubernetes_node":         tableScalewayKubernetesNode(ctx),
 			"scaleway_kubernetes_pool":         tableScalewayKubernetesPool(ctx),
